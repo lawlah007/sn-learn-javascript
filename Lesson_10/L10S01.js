e69030dc0bc841eeb43db8c554a691c489a12bf4
Omolola Adebayo <80699001+lawlah007@users.noreply.github.com>
2023-04-03 14:34:58 -0600

    My example

--- Lesson_10/L10S01.js
+++ Lesson_10/L10S01.js
@@ -9,3 +9,15 @@ var n = '3';
 var i = 3;
 gs.info(n == i); // REALLY?!!
 gs.info(i = 4); // WATCH OUT!!!
+
+//My example is below
+var a = 25;
+var b = 7;
+var c = 12;
+if (a < b) {
+   gs.info ('a is less than b');
+}  else if (a > b) {
+   gs.info ('a is greater than b');
+}  else {
+   gs.info ('it looks like a is equal to b');
+}
